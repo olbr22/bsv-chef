abd24ea3c1c3b055fe22d481a0738a1b4cb24832
Olha Bruce <olbr22@student.bth.se>
2024-05-20 19:14:00 +0200

    test

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,27 @@
 import pytest
+from unittest.mock import MagicMock, patch
 
-# add your test case implementation here
-@pytest.mark.unit
-def test():
-    pass
+from src.controllers.recipecontroller import RecipeController
+
+class TestRecipeControllerGetRecipe:
+    @pytest.fixture
+    def sut(self):
+        mockedDao = MagicMock()
+        mockedDao.find.return_value = None
+        sut = RecipeController(dao=mockedDao)
+        sut.load_recipes = MagicMock()
+        sut.load_recipes.return_value = [{'name': 'test', 'diet': 'normal', 'ingredients': ['test']}]
+        return sut
+
+    @pytest.mark.unit
+    def test_get_recipe_1(self, sut):
+        """
+        Test case #1 
+        """
+        with patch('src.controllers.recipecontroller.get_readiness_of_recipes', autospec=True) as mockedreadiness, \
+                patch('src.controllers.recipecontroller.random.randint', autospec=True) as mockedrecipeindex:
+            mockedreadiness.return_value = None
+            mockedrecipeindex.return_value = 0
+        
+        result = sut.get_recipe('normal', True)
+        assert result == {'name': 'test', 'diet': 'normal', 'ingredients': ['test']}
